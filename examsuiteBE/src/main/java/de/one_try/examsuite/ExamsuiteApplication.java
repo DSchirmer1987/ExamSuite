e635bf16f025bc63e7892d4e715e654ffe9c18a2
Daniel Schirmer <d.schirmer1987@gmail.com>
2024-08-19 10:30:23 +0200

    first commit

---
+++ examsuiteBE/src/main/java/de/one_try/examsuite/ExamsuiteApplication.java
@@ -0,0 +1,13 @@
+package de.one_try.examsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsuiteApplication.class, args);
+	}
+
+}
---
+++ examsuiteBE/src/test/java/de/one_try/examsuite/ExamsuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.one_try.examsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
